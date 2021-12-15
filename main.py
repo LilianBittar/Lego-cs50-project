f72eb592472e998a837dee16b614232550151778
Maxim-Zavidei <maxim-zavidei+github@pm.me>
2021-12-15 19:48:14 +0100

    commit

---
+++ main.py
@@ -0,0 +1,142 @@
+#!/usr/bin/env pybricks-micropython
+
+from pybricks.tools import wait
+from pybricks.hubs import EV3Brick
+from pybricks.ev3devices import Motor, TouchSensor, InfraredSensor
+from pybricks.media.ev3dev import SoundFile
+from pybricks.parameters import Button, Direction, Port, Stop
+
+from threading import Thread
+
+class Snake:
+
+    def __init__(self, ir_sensor_port: Port, ir_beacon_channel: int, driving_motor_port: Port, steering_motor_port: Port, striking_motor_port: Port):
+        self.ev3_brick = EV3Brick()
+        self.ir_sensor = InfraredSensor(ir_sensor_port)
+        self.ir_beacon_channel = ir_beacon_channel
+        self.driving_motor = Motor(driving_motor_port, Direction.CLOCKWISE)
+        self.steering_motor = Motor(steering_motor_port, Direction.CLOCKWISE)
+        self.striking_motor = Motor(striking_motor_port, Direction.CLOCKWISE)
+
+        self.behaviour_state = AutonomousState()
+
+        self.left_end = self.steering_motor.run_until_stalled(-200, then=Stop.HOLD)
+        self.right_end = self.steering_motor.run_until_stalled(200, then=Stop.HOLD)
+
+        print(self.left_end)
+        print(self.right_end)
+
+        self.limit = (self.right_end - self.left_end) // 2
+
+        print(self.limit)
+        self.steering_motor.reset_angle(self.limit)
+        self.steering_motor.run_target(speed=200, target_angle=0, then=Stop.COAST)
+
+
+    def run(self):
+        self.behaviour_state.run(self)
+
+
+    def hiss(self):
+        self.ev3_brick.speaker.play_file(file=SoundFile.SNAKE_HISS)
+
+
+    def strike(self):
+        self.striking_motor.run_time(220, 1100)
+        self.striking_motor.run_time(-220, 1100)
+
+
+    def bitting_action_behaviour(self):
+        queue = []
+
+        print("gg")
+        for i in range(10):
+            queue.append(self.ir_sensor.distance())
+        
+        while True:
+            dist = self.ir_sensor.distance()
+            avg = 0
+            for value in queue:
+                avg += value
+            avg /= len(queue)
+
+            print(avg)
+            print(queue)
+
+            if abs(avg - dist) > 70:
+                strike(self)
+                hiss(self)
+                queue.clear()
+                for i in range(10):
+                    queue.append(self.ir_sensor.distance())
+            else:
+                queue.pop(0)
+                queue.append(value)
+            
+
+
+class ManualState:
+    def run(self, snake: Snake):
+        while True:
+            ir_beacons_pressed = set(snake.ir_sensor.buttons(snake.ir_beacon_channel))
+
+            if ir_beacons_pressed == {Button.LEFT_UP, Button.RIGHT_UP}:
+                snake.driving_motor.run(1000)
+
+            elif ir_beacons_pressed == {Button.LEFT_DOWN, Button.RIGHT_DOWN}:
+                snake.driving_motor.run(-1000)
+
+            elif ir_beacons_pressed == {Button.LEFT_UP}:
+                snake.steering_motor.run(-500)
+                snake.driving_motor.run(1000)
+
+            elif ir_beacons_pressed == {Button.RIGHT_UP}:
+                snake.steering_motor.run(500)
+                snake.driving_motor.run(1000)
+
+            elif ir_beacons_pressed == {Button.LEFT_DOWN}:
+                snake.steering_motor.run(-500)
+                snake.driving_motor.run(-1000)
+
+            elif ir_beacons_pressed == {Button.RIGHT_DOWN}:
+                snake.steering_motor.run(500)
+                snake.driving_motor.run(-1000)
+
+            else:
+                snake.steering_motor.hold()
+                snake.driving_motor.stop()
+
+
+class AutonomousState:
+    def run(self, snake: Snake):
+        queue = []
+
+        for i in range(10):
+            queue.append(snake.ir_sensor.distance())
+        
+        last = snake.ir_sensor.distance()
+        while True:
+
+            wait(200)
+            dist = snake.ir_sensor.distance()
+
+            avg = 0
+            for value in queue:
+                avg += value
+            avg /= len(queue)
+
+            print(avg - dist)
+
+            if avg - dist > 25:
+                snake.hiss()
+                snake.strike()
+                queue.clear()
+                for i in range(10):
+                    queue.append(snake.ir_sensor.distance())
+            else:
+                queue.pop(0)
+                queue.append(value)
+
+
+snake = Snake(Port.S4, 1, Port.A, Port.C, Port.B)
+snake.run()
