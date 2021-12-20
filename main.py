9b30ceae9c7d93f12076c3f58e240476fc7f0247
Maxim-Zavidei <maxim-zavidei+github@pm.me>
2021-12-20 12:43:17 +0100

    added random wiggle

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env pybricks-micropython
+
 from pybricks.hubs import EV3Brick
-from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor,
-                                 InfraredSensor, UltrasonicSensor, GyroSensor)
+from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor, InfraredSensor, UltrasonicSensor, GyroSensor)
 from pybricks.parameters import Port, Stop, Direction, Button, Color
 from pybricks.tools import wait, StopWatch, DataLog
 from pybricks.robotics import DriveBase
 from pybricks.media.ev3dev import SoundFile, ImageFile
 
 from threading import Thread
+import random
 
 class Snake:
 
@@ -22,6 +23,7 @@ class Snake:
         self.behaviour_state = AutonomousState()
 
         self.to_wiggle = True
+        self.to_move = True
 
         self.left_end = self.steering_motor.run_until_stalled(-200, then=Stop.HOLD)
         self.right_end = self.steering_motor.run_until_stalled(200, then=Stop.HOLD)
@@ -35,6 +37,8 @@ class Snake:
         self.steering_motor.reset_angle(self.limit)
         self.steering_motor.run_target(speed=200, target_angle=0, then=Stop.COAST)
 
+        self.striking_motor.run_time(-220, 1100)
+
 
     def run(self):
         self.behaviour_state.run(self)
@@ -43,23 +47,25 @@ class Snake:
     def hiss(self):
         self.ev3_brick.speaker.play_file(file=SoundFile.SNAKE_HISS)
 
+
     def strike(self):
         self.striking_motor.run_time(220, 1100)
         self.striking_motor.run_time(-220, 1100)
 
+
     def wiggle_behaviour(self):
         while True:
             if self.to_wiggle:
-                self.steering_motor.run_until_stalled(-200, then=Stop.HOLD)
+                self.steering_motor.run_until_stalled(random.random() * 100 + 20, then=Stop.HOLD)
                 wait(1000)
-                self.steering_motor.run_until_stalled(200, then=Stop.HOLD)
+                self.steering_motor.run_until_stalled(-(random.random() * 100 + 20), then=Stop.HOLD)
                 wait(1000)
+
 
     def bitting_action_behaviour(self):
         queue = []
 
-        print("gg")
-        for i in range(5):
+        for i in range(10):
             queue.append(self.ir_sensor.distance())
         
         while True:
@@ -69,20 +75,22 @@ class Snake:
                 avg += value
             avg /= len(queue)
 
-            print(avg)
-            print(queue)
-
-            if abs(avg - dist) > 70:
-                strike(self)
-                hiss(self)
+            if abs(avg - dist) > 40:
+                self.strike()
+                self.hiss()
                 queue.clear()
                 for i in range(10):
                     queue.append(self.ir_sensor.distance())
             else:
                 queue.pop(0)
                 queue.append(value)
-            
 
+    def move_forward(self):
+        while True:
+            if self.to_move:
+                snake.driving_motor.run(random.random() * 200 + 100)
+                
+            
 
 class ManualState:
     def run(self, snake: Snake):
@@ -118,6 +126,10 @@ class ManualState:
 
 class AutonomousState:
     def run(self, snake: Snake):
+
+        Thread(target=snake.wiggle_behaviour).start()
+        Thread(target=snake.bitting_action_behaviour).start()
+
         snake.driving_motor.run(500)
         while True:
             if snake.ir_sensor.distance() <= 50:
@@ -131,10 +143,9 @@ class AutonomousState:
                 else:
                     snake.steering_motor.run_until_stalled(-200, then=Stop.HOLD)
                     snake.driving_motor.run(500)   
+            else:
                 snake.to_wiggle = True
                 
                 
 snake = Snake(Port.S4, 1, Port.A, Port.C, Port.B)
-t = Thread(target=snake.wiggle_behaviour)
-t.start()
 snake.run()
